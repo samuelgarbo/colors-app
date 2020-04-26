1479f7b4d6e3d68c9c4debcf675da11e3a412650
Samuel Garbo <samuel.garbo@gmail.com>
2020-04-26 23:00:55 +0300

    created react app, added palette

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Palette from './Palette';
+import seedColors from './seedColors';
+
+function App() {
+  return (
+    <div>
+      	<Palette></Palette>
+    </div>
+  );
+}
+
+export default App;
