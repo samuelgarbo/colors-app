8c92b29a37858b02634b549e3f34634d52b63cc8
Samuel Garbo <samuel.garbo@gmail.com>
2020-05-07 17:14:07 +0300

    Refactor ColorBox styles

--- src/Palette.js
+++ src/Palette.js
@@ -29,7 +29,7 @@ class Palette extends Component {
             key={color.id}
             id={color.id}
             paletteId={id}
-            showLink={true}
+            showFullPalette={true}
             ></ColorBox>
         ));
         return (
