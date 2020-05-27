709115fd374464a4d5890fc965dc4845e0b26c30
Samuel Garbo <samuel.garbo@gmail.com>
2020-05-28 00:44:03 +0300

    Save palettes to localStorage

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ class App extends Component {
   };
   constructor(props){
     super(props);
+    const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'));
     this.state = {
-      palettes: seedColors,
+      palettes: savedPalettes || seedColors,
 
     }
     this.savePalette = this.savePalette.bind(this);
@@ -29,7 +30,14 @@ class App extends Component {
       })
     }
     savePalette(newPalette){
-      this.setState({palettes: [...this.state.palettes, newPalette]});
+      this.setState({palettes: [...this.state.palettes, newPalette]}, this.syncLocalStorage );
+    }
+    syncLocalStorage(){
+      //save palettes to local storage
+      window.localStorage.setItem(
+        'palettes', 
+        JSON.stringify(this.state.palettes)
+        );
     }
     render(){
       return (
